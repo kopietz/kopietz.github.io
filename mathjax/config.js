5a29325d5b18975488791c739ce15df480968099
Andreas Daniel Kopietz <kopietz@gmail.com>
2015-01-25 07:27:41 +0100

    Create config.js

---
+++ mathjax/config.js
@@ -0,0 +1,9 @@
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [ ['$','$'], ['\\(','\\)'] ],
+    displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+    processEscapes: true,
+  }
+});
+
+MathJax.Ajax.loadComplete("http://dev.kopietz.org/mathjax/config.js");
