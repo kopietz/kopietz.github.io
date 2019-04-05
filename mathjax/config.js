f52dbca27db976ced8b58c111e3b417786e2d1be
Andreas Daniel Kopietz <kopietz@gmail.com>
2019-04-05 18:46:07 +0200

    Delete config.js

--- mathjax/config.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-  tex2jax: {
-    inlineMath: [ ['$','$'], ['\\(','\\)'] ],
-    displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
-    processEscapes: true,
-  }
-});
-
-MathJax.Ajax.loadComplete("http://git.adk.systems/mathjax/config.js");
